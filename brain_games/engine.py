07deb4488856cd1958cc350383400c181085b58a
DSemenko <me@dsemenko.ru>
2020-03-07 17:02:25 +0300

    Made engine.py code simpler

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,20 +5,15 @@ def run(game, rounds=3):
     print('Welcome to the Brain Games!')
     print(game.DESCRIPTION)
     name = cli.welcome_user()
-    correct_answers = 0
-    user_answer = None
-    while correct_answers < rounds:
+    for round in range(rounds):
         question, answer = game.start()
         user_answer = input(f'Question: {question} ')
         if user_answer == answer:
             print('Correct!')
-            correct_answers += 1
         else:
-            break
-        user_answer = None
-    if correct_answers == 3:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"'{user_answer}' is wrong answer ;( Correct answer was '{answer}'.\
+            print(f"'{user_answer}' is wrong answer ;( Correct answer was '{answer}'.\
             ")
-        print(f"Let's try again, {name}!")
+            print(f"Let's try again, {name}!")
+            break
+    else:
+        print(f'Congratulations, {name}!')
